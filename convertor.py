c2495c26326d9bf15775b4d55844b7ecca5df2b9
Nick <nifuller@protonmail.com>
2023-12-21 09:58:28 -0800

    updated gui; working open,save,convert bttns; cleared clutter in convertor

--- convertor.py
+++ convertor.py
@@ -7,70 +7,44 @@ Need to test longer pdf files
 """
 from pdfminer.high_level import extract_text
 from openai import OpenAI
-from tkinter import Tk
-from tkinter.filedialog import askopenfilename
-
-# intro = """
-# ██████╗ ██████╗ ███████╗    ████████╗ ██████╗     ██╗  ██╗███╗   ███╗██╗          ██████╗ ███████╗███╗   ██╗███████╗██████╗  █████╗ ████████╗ ██████╗ ██████╗ 
-# ██╔══██╗██╔══██╗██╔════╝    ╚══██╔══╝██╔═══██╗    ╚██╗██╔╝████╗ ████║██║         ██╔════╝ ██╔════╝████╗  ██║██╔════╝██╔══██╗██╔══██╗╚══██╔══╝██╔═══██╗██╔══██╗
-# ██████╔╝██║  ██║█████╗         ██║   ██║   ██║     ╚███╔╝ ██╔████╔██║██║         ██║  ███╗█████╗  ██╔██╗ ██║█████╗  ██████╔╝███████║   ██║   ██║   ██║██████╔╝
-# ██╔═══╝ ██║  ██║██╔══╝         ██║   ██║   ██║     ██╔██╗ ██║╚██╔╝██║██║         ██║   ██║██╔══╝  ██║╚██╗██║██╔══╝  ██╔══██╗██╔══██║   ██║   ██║   ██║██╔══██╗
-# ██║     ██████╔╝██║            ██║   ╚██████╔╝    ██╔╝ ██╗██║ ╚═╝ ██║███████╗    ╚██████╔╝███████╗██║ ╚████║███████╗██║  ██║██║  ██║   ██║   ╚██████╔╝██║  ██║
-# ╚═╝     ╚═════╝ ╚═╝            ╚═╝    ╚═════╝     ╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝     ╚═════╝ ╚══════╝╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝
-                                                                                                                                                              
-# ██████╗  ██████╗ ██╗    ██╗███████╗██████╗ ███████╗██████╗     ██████╗ ██╗   ██╗     ██████╗██╗  ██╗ █████╗ ████████╗ ██████╗ ██████╗ ████████╗               
-# ██╔══██╗██╔═══██╗██║    ██║██╔════╝██╔══██╗██╔════╝██╔══██╗    ██╔══██╗╚██╗ ██╔╝    ██╔════╝██║  ██║██╔══██╗╚══██╔══╝██╔════╝ ██╔══██╗╚══██╔══╝               
-# ██████╔╝██║   ██║██║ █╗ ██║█████╗  ██████╔╝█████╗  ██║  ██║    ██████╔╝ ╚████╔╝     ██║     ███████║███████║   ██║   ██║  ███╗██████╔╝   ██║                  
-# ██╔═══╝ ██║   ██║██║███╗██║██╔══╝  ██╔══██╗██╔══╝  ██║  ██║    ██╔══██╗  ╚██╔╝      ██║     ██╔══██║██╔══██║   ██║   ██║   ██║██╔═══╝    ██║                  
-# ██║     ╚██████╔╝╚███╔███╔╝███████╗██║  ██║███████╗██████╔╝    ██████╔╝   ██║       ╚██████╗██║  ██║██║  ██║   ██║   ╚██████╔╝██║        ██║                  
-# ╚═╝      ╚═════╝  ╚══╝╚══╝ ╚══════╝╚═╝  ╚═╝╚══════╝╚═════╝     ╚═════╝    ╚═╝        ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝    ╚═════╝ ╚═╝        ╚═╝                                                                                                                                                                               
-# """
-
-# print(intro)
-
-
-#opens up a window for user to select pdf file
-Tk().withdraw()
-file_path = askopenfilename()
 
 #convert pdfs to a string
-pdf_text = extract_text(file_path)
+def pdf_conversion(file_path):
+    pdf_text = extract_text(file_path)
+    return pdf_text
 
 
+def xml_conversion(pdf_text, user_prompt):
+    #restricts the user to only XML related conversions
+    assistant_prompt = """
+    You are a helpful assistant. You only convert user 
+    pdf_text strings to xml. Only accept convert to xml 
+    prompts.Have an underscore between names. Always
+    include xml header.
+    """
 
-prompt_example = """
-====================================================================================================================================================
-||                                                            EXAMPLE PROMPT                                                                      ||
-====================================================================================================================================================
-                        'convert to xml: medical, patient, last_name first_name, payment, payment_amount, payment_date'
-"""
-print(prompt_example)
+    client = OpenAI()
+    stream = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages= [{"role": "system", "content": assistant_prompt},
+                {"role": "user", "content": user_prompt},
+                {"role": "assistant", "content": pdf_text}
+                ],
+        stream=True,
+        max_tokens=256,
+        n=1,
+        stop=None
+    )
+    return stream
 
-#grab user's input. should only be entering xml related prompts
-user_prompt = input('Enter prompt command:')
+def display_response(stream):
+    for chunk in stream:
+        if chunk.choices[0].delta.content is not None:
+            print(chunk.choices[0].delta.content, end="")
 
-#restricts the user to only XML related conversions
-assistant_prompt = """
-You are a helpful assistant. You only convert user 
-pdf_text strings to xml. Only accept convert to xml
-prompts. User may use element to describe xml elements.
-"""
-
-client = OpenAI()
-stream = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages= [{"role": "system", "content": assistant_prompt},
-               {"role": "user", "content": "xml version and endcoding" + user_prompt},
-               {"role": "assistant", "content": pdf_text}
-              ],
-    stream=True,
-    max_tokens=256,
-    n=1,
-    stop=None
-)
-file_name = input('Enter file name to save:')
-file = open(file_name + ".xml", "w", encoding="utf-8")
-for chunk in stream:
-    if chunk.choices[0].delta.content is not None:
-        file.write(chunk.choices[0].delta.content)
-        print(chunk.choices[0].delta.content, end="")
+def save_xml_file(stream, xml_file_name):
+    #file_name = input('Enter file name to save:')
+    file = open(xml_file_name, "w", encoding="utf-8")
+    for chunk in stream:
+        if chunk.choices[0].delta.content is not None:
+            file.write(chunk.choices[0].delta.content)
--- gui.py
+++ gui.py
@@ -1,43 +1,62 @@
-from tkinter import *
-from tkinter.ttk import *
+import tkinter as tk
+from tkinter.filedialog import askopenfilename, asksaveasfile
 from tkinter import filedialog
+import convertor 
 
-def browseFiles():
-    filename = filedialog.askopenfilename(initialdir = "/",
-                                          title = "select a file",
-                                          filetypes = (("PDF files",
-                                                        "*.pdf"),
-                                                        ("all files",
-                                                         "*.*")))
-    #label_file_explore.configure(text="file opened: "+ filename)
-    return filename
+def browse_files():
+    global filename
+    filename = filedialog.askopenfilename(initialdir="/",
+                                           title="select a file",
+                                           filetypes=(("PDF files",
+                                                       "*.pdf"),
+                                                       ("all files",
+                                                        "*.*")))
+    lbl_filename["text"] = filename
 
-# root = Tk()
+def convert_pdf():
+    global response
+    pdf_text = convertor.pdf_conversion(filename)
+    usr_prompt = entry_text.get("1.0", tk.END)
+    response = convertor.xml_conversion(pdf_text, usr_prompt)
 
-# root.title("PDF to XML Converter")
-# root.geometry("350x200")
-# root.config(background = "white")
 
-# label_file_explore = Label(root,
-#                            text = "File Explorer")
+def save_xml():
+    files = [('All Files', '*'),
+             ('Python Files', '*.py'),
+             ('XML Files', '*.xml')]
+    file = asksaveasfile(filetypes=files, defaultextension=".xml")
+    print(file.name)
+    convertor.save_xml_file(response, file.name)
 
-# def button():
-#     button_explore = Button(root,
-#                             text = "browse files",
-#                             command = browseFiles).grid(column=1, row=2)
+window = tk.Tk()
+window.title("PDF Extractor")
 
-# def Close():
-#     root.destroy()
+window.rowconfigure(0, minsize=200, weight=1)
+window.columnconfigure(1, minsize=200, weight=1)
 
-# button_exit = Button(root,
-#                      text = "exit",
-#                      command= Close)
+menu_buttons = tk.Frame(window, relief=tk.RAISED, bd=2)
+prompt_frame = tk.Frame(window, padx=10, pady=5)
 
-# label_file_explore.grid(column = 1, row = 1)
+entry_text = tk.Text(prompt_frame, height=10, width=50)
+entry_text.grid(row=1, column=0)
 
-# button()
+lbl_prompt = tk.Label(prompt_frame, text="PROMPT ENTRY")
+lbl_prompt.grid(row=0, column=0, sticky="nw")
 
-# button_exit.grid(column = 1, row = 3)
+lbl_filename = tk.Label(master=window, text="Test")
+lbl_filename.grid(row=4, column=1, stick="w")
 
-    
-# root.mainloop()
+btn_open = tk.Button(menu_buttons, text="Open", command=browse_files)
+btn_open.grid(row=0, column=1, sticky="ew", padx=5, pady=5)
+
+btn_convert = tk.Button(menu_buttons, text="Convert", command=convert_pdf)
+btn_convert.grid(row=0, column=2, sticky="ew")
+
+btn_save = tk.Button(menu_buttons, text="Save", command=save_xml)
+btn_save.grid(row=0, column=3, sticky="ew")
+
+
+menu_buttons.grid(row=1, column=1, sticky="sw")
+prompt_frame.grid(row=0, column=1)
+
+window.mainloop()
